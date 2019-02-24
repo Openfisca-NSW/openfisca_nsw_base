c7fff2edc00cda25d212dde74374fa50a3234261
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-25 12:18:18 +1300

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "OpenFisca-nsw",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.org",
     classifiers=[
