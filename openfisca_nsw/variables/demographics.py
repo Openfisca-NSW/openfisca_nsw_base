e86e737a8b1185ceb664e148e9c1cf3b44bdc331
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-02-26 13:49:35 +1300

    Age in months and years working

--- openfisca_nsw/variables/demographics.py
+++ openfisca_nsw/variables/demographics.py
@@ -43,17 +43,31 @@ class age_in_months(Variable):
     value_type = int
     entity = Person
     definition_period = MONTH
-    label = u"Person's age (in years)"
+    label = u"Person's age (in months)"
 
     # A person's age is computed according to its birth date.
     def formula(persons, period, parameters):
-        birth = persons('birth', period)
-        birth_month = birth.astype('datetime64[M]').astype(int) % 12 + 1
-
-        birth_day = (birth - birth.astype('datetime64[M]') + 1).astype(int)
-        is_birthday_past = (birth_month < period.start.month) + (birth_month == period.start.month) * (birth_day <= period.start.day)
-
+        is_birthday_past = persons('is_birthday_past', period)
         age_in_years = persons('age', period) + where(is_birthday_past, 0, 1)
+        birth_month = persons('birth_month', period)
         return (age_in_years * 12) + (period.start.month - birth_month)
 
 
+class is_birthday_past(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+
+    def formula(persons, period, parameters):
+        birth = persons('birth', period)
+        birth_month = persons('birth_month', period)
+        birth_day = (birth - birth.astype('datetime64[M]') + 1).astype(int)
+        return (birth_month < period.start.month) + (birth_month == period.start.month) * (birth_day <= period.start.day)
+
+class birth_month(Variable):
+    value_type = int
+    entity = Person
+    definition_period = ETERNITY  # This variable cannot change over time.
+
+    def formula(persons, period, parameters):
+        return persons('birth', period).astype('datetime64[M]').astype(int) % 12 + 1
