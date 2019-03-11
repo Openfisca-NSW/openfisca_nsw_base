c94b3eaa6a242984f3643a729671bc18274dd2d8
asgharkazi <asghar.kazi@gmail.com>
2019-03-11 11:22:50 +1100

    Update entities.py

--- openfisca_nsw/entities.py
+++ openfisca_nsw/entities.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 # -*- coding: utf-8 -*-
 
 """
@@ -5,6 +11,8 @@
  """
 from openfisca_core.entities import build_entity
 
+# Assigning roles within a family unit, namely "Family Eligible Person" and other, however if there is no need, the
+# Family (Group entity) can be removed.
 Family = build_entity(
     key = "family",
     plural = "families",
@@ -13,24 +21,16 @@ Family = build_entity(
     ''',
     roles = [
         {
-            'key': 'parent',
-            'plural': 'parents',
-            'label': u'Parents',
-            'max': 2,
-            'subroles': ['parent', 'carer', 'guardian'],
-            'doc': u'The one or two adults responsible for children in the family.'
-            },
-        {
-            'key': 'child',
-            'plural': 'children',
-            'label': u'Child',
-            'doc': u'Children of the parents, carers, or guardians.'
+            'key': 'FTB Eligible Person',
+            'plural': 'FTB Eligible Persons',
+            'label': u'FTB Eligible Person',
+            'doc': u'Any member of the family who is registered as the FTB Recipient.'
             },
         {
             'key': 'other',
             'plural': 'others',
             'label': u'Other',
-            'doc': u'Other individuals living in the household.'
+            'doc': u'Other individuals living in the household who are not registered as the FTB Recipients.'
             }
         ]
     )
@@ -40,15 +40,12 @@ Person = build_entity(
     plural = "persons",
     label = u'An individual. The minimal legal entity on which a legislation might be applied.',
     doc = '''
-
     Variables like 'salary' and 'income_tax' are usually defined for the entity 'Person'.
-
     Usage:
     Calculate a variable applied to a 'Person' (e.g. access the 'salary' of a specific month with
     person('salary', "2017-05")).
     Check the role of a 'Person' in a group entity (e.g. check if a the 'Person' is a 'first_parent'
     in a 'Family' entity with person.has_role(Family.PARENT)).
-
     For more information, see: https://openfisca.org/doc/coding-the-legislation/50_entities.html
     ''',
     is_person = True,
