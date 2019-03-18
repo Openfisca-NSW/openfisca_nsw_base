10cc17fbea61e2c3e5df80e812d09daf051f3653
asgharkazi <asghar.kazi@gmail.com>
2019-03-18 16:00:55 +1100

    Update energy_account.py
    Added energy related variables to a single file.

--- openfisca_nsw/variables/energy_account.py
+++ openfisca_nsw/variables/energy_account.py
@@ -9,3 +9,9 @@ class has_energy_account(Variable):
     entity = Person
     definition_period = MONTH
     label = "registered an energy account in their own name"
+
+class energy_provider_category(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "Does the applicant receive their energy bill from a strata manager/community village operator?"
