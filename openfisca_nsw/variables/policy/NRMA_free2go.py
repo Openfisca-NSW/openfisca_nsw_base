72efb8e99cdc222ab58a4909964626799b5371ff
TJ <tjharrop@gmail.com>
2019-03-22 10:42:31 +1100

    added ACT resi

---
+++ openfisca_nsw/variables/policy/NRMA_free2go.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# This file defines variables for the modelled legislation.
+# A variable is a property of an Entity such as a Person, a Household…
+# See https://openfisca.org/doc/key-concepts/variables.html
+
+# Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the Entities specifically defined for this tax and benefit system
+from openfisca_nsw.entities import *
+
+class NRMA_free2go__is_eligible(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "Person is eligible for NRMA free2go"
+
+class NRMA_free2go__is_NRMA_member(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "Person is an active NRMA member"
