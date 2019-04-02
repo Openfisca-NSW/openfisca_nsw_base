e55b10c6b64477231e73970d73a2939113d4d421
TJ <tjharrop@gmail.com>
2019-04-02 13:34:19 +1100

    Refactored Teenage Education Payments to use family model

--- openfisca_nsw/variables/education.py
+++ openfisca_nsw/variables/education.py
@@ -9,3 +9,10 @@ class is_enrolled_in_school(Variable):
     entity = Person
     definition_period = MONTH
     label = "enrolled in school (from Kindergarten to Year 12, including those who are home-schooled or enrolled in secondary school education at TAFE NSW)"
+
+
+class is_enrolled_in_nsw_school(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "The school the person is enrolled in is in NSW"
