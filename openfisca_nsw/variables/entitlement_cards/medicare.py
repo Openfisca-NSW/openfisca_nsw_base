c18ecad0704c6fc3047ec85e91e2d623cf0153c5
ugoharry <ugo.harry@gmail.com>
2019-04-05 16:22:13 +1100

    Created test file, added variables

--- openfisca_nsw/variables/entitlement_cards/medicare.py
+++ openfisca_nsw/variables/entitlement_cards/medicare.py
@@ -9,3 +9,17 @@ class has_valid_medicare_card(Variable):
     entity = Person
     definition_period = MONTH
     label = "has a valid Medicare card"
+
+
+class has_stayed_in_Out_Of_Care_Home(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "has been in Out of Home Care (OOHC)"
+
+
+class currently_staying_in_Out_Of_Care_Home(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = "Is currently in Out of Home Care (OOHC)"
