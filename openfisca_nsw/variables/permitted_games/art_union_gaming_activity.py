2f6a19e97587d51edc1375b48c7e1c795b7749af
Asghar Hussain Kazi <asghar.kazi@gmail.com>
2019-04-26 12:21:31 +1000

    Added code for art union gaming activity & added gaming activity types

---
+++ openfisca_nsw/variables/permitted_games/art_union_gaming_activity.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# This file defines variables for the modelled legislation.
+# A variable is a property of an Entity such as a Person, an Organisation…
+# See https://openfisca.org/doc/key-concepts/variables.html
+
+# Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the Entities specifically defined for this tax and benefit system
+from openfisca_nsw.entities import *
+
+
+# This is used to calculate whether an organisation is permitted to conduct a guessing games
+class art_union__game_meets_criteria(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "The eligibility conditions for organising an art union gaming activity are being met by the organisation"
+
+    def formula(organisation, period, parameters):
+        return (
+            (organisation('is_art_union', period)
+            * (organisation('proceeds_to_benefitting_organisation', period)) >= ((organisation('gross_proceeds_from_gaming_activity', period) * parameters(period).permitted_games.art_union_gaming_activity.min_gross_proceeds_percent_to_benefit_org))))
---
+++ openfisca_nsw/variables/permitted_games/gaming_activity_types.py
@@ -0,0 +1,46 @@
+# Import from openfisca-core the common Python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the Entities specifically defined for this tax and benefit system
+from openfisca_nsw.entities import *
+
+
+class gaming_activity_is_guessing_competition(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Whether organisation is conducting a guessing competition?"
+
+
+class gaming_activity_is_draw_lottery(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Whether organisation is conducting a draw lottery?"
+
+
+class gaming_activity_is_free_lottery(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Whether organisation is conducting a free lottery?"
+
+
+class gaming_activity_is_progressive_lottery(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Whether organisation is conducting a progressive lottery?"
+
+
+class gaming_activity_is_mini_numbers_lottery(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Whether organisation is conducting a mini-numbers lottery?"
+
+
+class gaming_activity_is_housie(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Whether organisation is conducting a housie?"
