3d80d4f9d8f0eabea3f0b513426fe45d98c3f5bb
Asghar Kazi <asghar.kazi@gmail.com>
2019-12-11 12:54:03 +1100

    Added new organisation types

--- openfisca_nsw/variables/permitted_games/organisation_type.py
+++ openfisca_nsw/variables/permitted_games/organisation_type.py
@@ -15,7 +15,7 @@ class is_not_for_profit(Variable):
     value_type = bool
     entity = Organisation
     definition_period = MONTH
-    label = "Organisation is a not-for-profit"
+    label = "Organisation is a non-profit"
 
 
 class is_art_union(Variable):
@@ -37,3 +37,38 @@ class is_for_profit_business(Variable):
     entity = Organisation
     definition_period = MONTH
     label = "Organisation is a for-profit business"
+
+
+class is_political_party(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Organisation is a political party"
+
+
+class is_trade_union(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Organisation is a trade union"
+
+
+class is_racing_club(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Organisation is a club registered under the Rules of Racing of Racing New South Wales"
+
+
+class is_greyhound_racing_club(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Organisation is a greyhound racing club within the meaning of the Greyhound Racing Act 2017"
+
+
+class is_harness_racing_club(Variable):
+    value_type = bool
+    entity = Organisation
+    definition_period = MONTH
+    label = "Organisation is a harness racing club within the meaning of the Harness Racing Act 2009"
